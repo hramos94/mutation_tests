64dfd3fa587f5c40718df121602597f3c2ecba46
hramos94 <heitorlramos@gmail.com>
2025-03-20 12:48:50 -0300

    ultimo commit

--- test_triangle.cpp
+++ test_triangle.cpp
@@ -4,31 +4,26 @@ extern "C"
 #include "triangle.h"
 }
 
-// Teste para um triângulo equilátero
 TEST(TriangleTypeTest, Equilateral)
 {
     EXPECT_STREQ(triangle_type(3, 3, 3), "Equilateral");
 }
 
-// Teste para um triângulo isósceles
 TEST(TriangleTypeTest, Isosceles)
 {
     EXPECT_STREQ(triangle_type(5, 5, 3), "Isosceles");
 }
 
-// Teste para um triângulo escaleno
 TEST(TriangleTypeTest, Scalene)
 {
     EXPECT_STREQ(triangle_type(3, 4, 5), "Scalene");
 }
 
-// Teste para um caso inválido (não forma um triângulo)
 TEST(TriangleTypeTest, NotATriangle)
 {
     EXPECT_STREQ(triangle_type(1, 2, 3), "Not a triangle");
 }
 
-// Teste para um caso limite de triângulo degenerado
 TEST(TriangleTypeTest, DegenerateTriangle)
 {
     EXPECT_STREQ(triangle_type(2, 2, 4), "Not a triangle");
--- triangle.h
+++ triangle.h
@@ -1,6 +1,14 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
-const char *triangle_type(int a, int b, int c);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+const char* triangle_type(int a, int b, int c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TRIANGLE_H
